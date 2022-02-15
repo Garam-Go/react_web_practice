5ce3a6007c7314d9f20dbcb5c395ac98294670c9
노건 <shrjs10@gmail.com>
2022-02-15 14:43:25 +0900

    구구단 웹팩 프리셋, 플러그인 설정
    바벨 preset-env에서
    브라우저 설정
    1. > 5% in KR : 한국에서 점유율 5퍼이상 브라우저
    2. last 2 chrome versions : 크롬 최신버전 2버전 아래까지
    
    웹팩 플러그인
    모든 웹팩 옵션에서 디버그 활성화

--- src/1. 구구단/webpack.config.js
+++ src/1. 구구단/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-
+const webpack = require('webpack')
 module.exports = {
   mode: 'development',
   devtool: 'eval', // hidden source map
@@ -16,11 +16,22 @@ module.exports = {
       test: /\.jsx?$/,
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-        // plugin: []
+        presets: [ // preset => plugin들의 모음
+          ['@babel/preset-env', { // 각 프리셋별로 설정 적용
+            targets: { // 자동으로 옛날 브라우저 지원
+              browsers: ['> 5% in KR','last 2 chrome versions'], // 원하는 브라우저만 적용
+              // 한국에서 점유율 5퍼이상, 크롬버전 최신 2버전 전 버전까지
+            },
+            debug: true,
+          }],
+          '@babel/preset-react'],
+        plugins: []
       }
     }],
   },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ debug: true })
+  ],
   output: {
     filename: 'app.js',
     path: path.join(__dirname, 'dist')
