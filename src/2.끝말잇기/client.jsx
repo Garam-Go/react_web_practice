74fb8c95d6cd881f7511737999d9a1301c073090
노건 <shrjs10@gmail.com>
2022-02-15 16:35:01 +0900

    끝말잇기 웹팩설정 및 빌드테스트 진행

---
+++ src/2.끝말잇기/client.jsx
@@ -0,0 +1,10 @@
+// import React from "react";
+// import ReactDom from "react-dom";
+// import Gugudan from "../1. 구구단/Gugudan";
+
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const WordRelay = require('./WordRelay');
+
+ReactDom.render(<WordRelay />, document.querySelector('#root'))
