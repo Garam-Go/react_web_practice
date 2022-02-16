761c2a5870955ff950882000efb9c6d6707c2468
노건 <shrjs10@gmail.com>
2022-02-16 14:38:31 +0900

    끝말잇기 내부 useState분리

--- src/2.끝말잇기/WordRelay.jsx
+++ src/2.끝말잇기/WordRelay.jsx
@@ -2,51 +2,46 @@ const React = require('react');
 const { useState, useRef, Fragment, useCallback } = require('react');
 
 const WordRelay = () => {
-  const [state, setState] = useState({
-    word: '리액트',
-    value: '',
-    result: ''
-  })
+  const [word, setWord] = useState('리액트')
+  const [value, setValue] = useState('')
+  const [result, setResult] = useState('')
 
   const inputEl = useRef(null)
 
   // input 입력시 state를 변하게 해주는 콜백함수
   const changeValue = useCallback((e) => {
     console.log('changeValue ::: ', e.target.value)
-    // input입력시 value값만 변하고 나머지는 그대로하도록 ...state사용
-    setState({...state, value: e.target.value})
-  }, [state])
+    // input입력시 저장된 value값 변경
+    setValue(e.target.value)
+  }, [])
 
   // 정답체크
   const onClickForm = useCallback(() => {
-    console.log('answer :::', state.value)
+    console.log('answer :::', value)
     
     // 입력된 글자의 첫번째가 이전단어 끝자리와 같을경우
-    if (state.value[0] === state.word[state.word.length -1]) {
-      setState({
-        result: '정답', // 결과 노출
-        word: state.value, // 문제 변경
-        value: '' // 입력된 정답 초기화
-      })
+    if (value[0] === word[word.length -1]) {
+
+      setResult('정답')// 결과 노출
+      setWord(value) // 문제 변경
+      setValue('') // 입력창 초기화
       // input창에 포커싱
       inputEl.current.focus()
     } else { 
       // 오답일경우
-      setState({
-        result: '땡', // 결과 노출
-        word: state.word,
-        value: '' // 입력된 정답 초기화
-      })
+      setResult('땡')// 결과 노출
+      setWord(word) // 문제 변경
+      setValue('') // 입력창 초기화
       // input창에 포커싱
       inputEl.current.focus()
     }
-  }, [state.value, state.word])
+  }, [value, word])
 
   const inputRenderer = useCallback(() => {
     const inputParam = {
       ref: inputEl,
       type: 'text',
-      value: state.value,
+      value: value,
       onChange: (e) => {
         changeValue(e)
       },
@@ -58,18 +53,18 @@ const WordRelay = () => {
       }
     }
     return (<input {...inputParam}/>)
-  }, [changeValue, onClickForm, state.value])
+  }, [changeValue, onClickForm, value])
 
   return (
     <Fragment>
       <div>
         <h1>끝말잇기 예제</h1>
-        <h2>제시어 : {state.word}</h2>
+        <h2>제시어 : {word}</h2>
         {inputRenderer()}
         <button onClick={onClickForm}>정답체크</button>
         <h2>핫로더 바로적용하기</h2>
       </div>
-      <div id={state.result}>{state.result}</div>
+      <div id={result}>{result}</div>
     </Fragment>
   )
 };
